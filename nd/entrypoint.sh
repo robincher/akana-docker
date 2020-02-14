11c768aec59868a9195f3f6bff8efe8e8cba3876
Ronald Kan <ronaldxkan@gmail.com>
2020-02-14 14:46:07 +0800

    added nd that respects statefulset

--- nd/entrypoint.sh
+++ nd/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sed -i "s/{hostname}/nd-$HOSTNAME/g" /opt/installer/nd-all.properties
 sed -i "s/{hostname}/nd-$HOSTNAME/g" /opt/installer/register-container.properties
+curl -X DELETE -u administrator:password http://bluewave-cm.intheapex.com:9900/rest/containers/nd-`echo $HOSTNAMEnd`
 /opt/akana/bin/jython.sh -Dorg.slf4j.simpleLogger.logFile=System.out -m akana.container --recipe /opt/akana/recipes/nd-all.json --props /opt/installer/nd-all.properties
 ps -ef | grep [j]ava | awk '{print $2}' | xargs kill -9
 sleep 10
