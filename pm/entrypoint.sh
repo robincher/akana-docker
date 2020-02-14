c625581c1dd488eef89ca17cad6a13e3ea960fbd
Ronald Kan <ronaldxkan@gmail.com>
2020-02-14 13:53:43 +0800

    refactored

--- pm/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sed -i "s/{hostname}/pm-$HOSTNAME/g" /opt/installer/pm-cm-all.properties
-sed -i "s/{hostname}/pm-$HOSTNAME/g" /opt/installer/add-to-local-cluster.properties
-sed -i "s/{clustername}/pmcluster/g" /opt/installer/add-to-local-cluster.properties
-/opt/akana/bin/jython.sh -Dorg.slf4j.simpleLogger.logFile=System.out -m akana.container --recipe /opt/akana/recipes/pm-all-existing-db.json --props /opt/installer/pm-cm-all.properties
-ps -ef | grep [j]ava | awk '{print $2}' | xargs kill -9
-sleep 10
-# /opt/akana/bin/jython.sh -Dorg.slf4j.simpleLogger.logFile=System.out -m akana.container --recipe /opt/akana/recipes/add-to-local-cluster.json --props /opt/installer/add-to-local-cluster.properties
-/opt/akana/bin/startup.sh pm-`echo $HOSTNAME`
